4bc1765d6538bbe8944802c6da13b7b5b1941828
locks13 <lktoffoli@gmail.com>
2021-12-09 17:32:24 -0300

    teste

---
+++ finland-project/src/Components/Header/index.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>                
+                    <li>Teste1</li>
+                    <li>Teste2</li>
+                    <li>Teste3</li>
+                </ul>
+                <div className="serc-bar">
+                    <img src="icon.svg" />
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
